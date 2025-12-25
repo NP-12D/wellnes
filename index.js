69280794666977130466a3822a0ea300e1ef17e8
Nona Pareulidze <nona.pareulidze1@gmail.com>
2025-12-25 11:14:49 +0400

    feat: added functionality

---
+++ index.js
@@ -0,0 +1,30 @@
+let burger = document.querySelector(".burger");
+let menu = document.querySelector("nav >div:nth-of-type(2)");
+let menu_content = document.querySelector(".menu_cont");
+let burger_listitems = document.querySelectorAll(".menu_cont> a");
+burger.addEventListener("click", burgermenu);
+burger_listitems.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (document.querySelector(".row_menu").classList.contains("menu")) {
+      burgermenu();
+    }
+  });
+});
+
+function burgermenu() {
+  menu.classList.toggle("menu");
+  menu_content.classList.toggle("menu_content");
+  document.body.classList.toggle("no-scroll");
+  document.querySelectorAll(".line").forEach((line) => {
+    line.style.backgroundColor =
+      line.style.backgroundColor === "white" ? "black" : "white";
+  });
+}
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 20) {
+    document.querySelector(".pages_list").style.position = "relative";
+  } else {
+    document.querySelector(".pages_list").classList.add(".pages_list");
+  }
+});
